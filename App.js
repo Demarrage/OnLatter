88c16780480b65286dac56b8175aa9a42a79dd67
GUILHERME VICTOR BOTA SALOMAO <guilherme.vbsalomao@senacsp.edu.br>
2021-05-20 12:04:38 -0300

    Inicio da criação do OnLatter

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { Text, View, TouchableOpacity } from "react-native";
+import { style } from "./css/Styles";
+import { AntDesign } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screens/Login";
+import Cadastro from "./screens/Cadastro";
+const pilha = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>ola</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <pilha.Navigator initialRoute="TelaInicial">
+        <pilha.Screen
+          name="TelaInicial"
+          component={TelaInicial}
+          options={{ headerShown: false }}
+        />
+        <pilha.Screen
+          name="Login"
+          component={Login}
+          options={{ headerTitleAlign: "center" }}
+        />
+        <pilha.Screen
+          name="Cadastro"
+          component={Cadastro}
+          options={{ headerTitleAlign: "center" }}
+        />
+      </pilha.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+function TelaInicial({ navigation }) {
+  return;
+}
